a84b52c68468ee59ed491baa3b2f2a90c855a177
DhariniAVijeykumar <139705470+DhariniAVijeykumar@users.noreply.github.com>
2026-03-22 11:33:23 +0530

    Create Dashboard.sql

---
+++ sql/Dashboard.sql
@@ -0,0 +1,45 @@
+--KPI - AVG Booking Value
+SELECT AVG(total_amount) as avg_booking_value
+FROM GOLD_BOOKING_CLEAN;
+
+--KPI - Total Guests
+SELECT SUM(NUM_GUESTS) AS TOTAL_GUESTS 
+FROM GOLD_BOOKING_CLEAN
+
+--KPI - Total Bookings
+SELECT COUNT(BOOKING_ID) AS TOTAL_BOOKINGS
+FROM GOLD_BOOKING_CLEAN;
+
+--KPI - Total Revenue
+SELECT SUM(TOTAL_AMOUNT) AS TOTAL_REVENUE
+FROM GOLD_BOOKING_CLEAN;
+
+--Line Chart - Monthly Revenue
+SELECT date, total_revenue
+FROM GOLD_AGG_DAILY_BOOKING
+ORDER BY date;
+-- For monthly revenue, in chart click the date drop down in bucketing select month
+
+--Line Chart - Monthly Bookings
+SELECT date, total_booking
+FROM GOLD_AGG_DAILY_BOOKING
+ORDER BY date;
+-- For monthly booking, in chart click date drop down, in bucketing select month
+
+--Bar Chart - Top Cities by Revenue
+SELECT hotel_city, total_revenue
+FROM GOLD_AGG_HOTEL_CITY_SALES
+WHERE total_revenue is NOT NULL
+ORDER BY total_revenue DESC
+LIMIT 5;
+
+--Bar Chart - Bookings by Status
+SELECT booking_status, COUNT(*) AS total
+FROM GOLD_BOOKING_CLEAN
+GROUP BY booking_status;
+
+--Bar Chart - Bookings by Type
+SELECT room_type, COUNT(*) AS total_bookings
+FROM GOLD_BOOKING_CLEAN
+GROUP BY room_type
+ORDER BY total_bookings DESC;
